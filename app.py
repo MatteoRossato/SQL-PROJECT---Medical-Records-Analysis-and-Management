22940fe7fea17a1ecf10815cebf443b73822c59a
MatteoRossato95 <matteorossato95@gmail.com>
2025-03-23 21:25:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+from flask import Flask, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# Connessione al database
+def query_db(query):
+    conn = sqlite3.connect("medical_records.db")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+@app.route("/patients", methods=["GET"])
+def get_patients():
+    data = query_db("SELECT * FROM Patients")
+    return jsonify(data)
+
+@app.route("/diagnoses", methods=["GET"])
+def get_diagnoses():
+    data = query_db("SELECT * FROM Diagnoses")
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
