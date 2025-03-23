2aae91cb9d6de5032da32b4a6420c4317fe62369
MatteoRossato95 <matteorossato95@gmail.com>
2025-03-23 21:24:22 +0100

    Update queries.sql

--- medical_records_sql/sql/queries.sql
+++ medical_records_sql/sql/queries.sql
@@ -1,1 +1,22 @@
+-- Patients Count
+SELECT COUNT(*) AS total_patients FROM Patients;
+
+-- Common diagnosis
+SELECT diagnosis, COUNT(*) AS occurrences 
+FROM Diagnoses 
+GROUP BY diagnosis 
+ORDER BY occurrences DESC;
+
+-- Average age 
+SELECT d.diagnosis, AVG(p.age) AS avg_age
+FROM Diagnoses d
+JOIN Patients p ON d.patient_id = p.patient_id
+GROUP BY d.diagnosis;
+
+-- Patients with multiple diagnosis
+SELECT p.name, COUNT(d.diagnosis) AS num_diagnoses
+FROM Patients p
+JOIN Diagnoses d ON p.patient_id = d.patient_id
+GROUP BY p.name
+HAVING num_diagnoses > 1;
 
